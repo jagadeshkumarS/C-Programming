4e3a52fa4fdf270797425230a78a91cc2c01b3fa
jagadeshkumarS <jagadeshs@univisiontechnocon.com>
2020-09-14 11:47:15 +0530

    Follows Conventions in DMA pgms

--- DMA/2D_array.c
+++ DMA/2D_array.c
@@ -2,34 +2,45 @@
 #include<stdlib.h>
 int main()
 {
-  int r,c,i,j;
+	int r,c,i,j;
+	int **p;
 
-  //Get number of rows and columns
-  printf("Enter the row and column value : ");
-  scanf("%d%d",&r,&c);
+	//Get number of rows and columns
+	printf("Enter the row and column value : ");
+	scanf("%d%d",&r,&c);
 
-  //Allocate DMA
-  int **p = (int **)malloc(r*sizeof(int *));
-  for(i=0;i<r;i++)
-    p[i] = (int *)malloc(c*sizeof(int));
+	//Allocate DMA
+	p = (int **)malloc(r*sizeof(int *));
+	for(i=0;i<r;i++)
+	{
+		p[i] = (int *)malloc(c*sizeof(int));
+	}
 
-  //Get and Display the Elements
-  printf("Enter the elements\n");
-  for(i=0;i<r;i++)
-  for(j=0;j<c;j++)
-    scanf("%d",&p[i][j]);
-  printf("Elements : \n");
-  for(i=0;i<r;i++)
-  {
-    for(j=0;j<c;j++)
-      printf("%d\t",p[i][j]);
-    printf("\n");
-  }
+	//Get and Display the Elements
+	printf("Enter the elements\n");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			scanf("%d",&p[i][j]);
+		}
+	}
+	printf("Elements : \n");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			printf("%d\t",p[i][j]);
+		}
+		printf("\n");
+	}
 
-  //De-Allocate the DMA
-  for(i=0;i<r;i++)
-    free(p[i]);
-  free(p);
+	//De-Allocate the DMA
+	for(i=0;i<r;i++)
+	{
+		free(p[i]);
+	}
+	free(p);
 
-  return 0;
+	return 0;
 }
--- DMA/realloc.c
+++ DMA/realloc.c
@@ -2,33 +2,35 @@
 #include<stdlib.h>
 int main()
 {
-  //Allocate DMA
-  int *p = (int *)malloc(5*sizeof(int));
+	//Allocate DMA
+	int *p,*a,*b;
+	p = (int *)malloc(5*sizeof(int));
 
-  printf("Before Realloc - p : %p\n",p);
-  printf("p[-1] : %d\n",p[-1]);
+	printf("Before Realloc - p : %p\n",p);
+	printf("p[-1] : %d\n",p[-1]);
 
-  p = (int *)realloc(p,10*sizeof(int));
+	p = (int *)realloc(p,10*sizeof(int));
   
-  printf("After Realloc - p : %p\n",p);
-  printf("p[-1] : %d\n",p[-1]);
+	printf("After Realloc - p : %p\n",p);
+	printf("p[-1] : %d\n",p[-1]);
 
-  //De-Allocate the DMA
-  free(p);
+	//De-Allocate the DMA
+	free(p);
 
-  //Allocate DMA
-  int *a = (int *)malloc(5*sizeof(int));
-  printf("Before Realloc - a : %p\n",a);
+	//Allocate DMA
+	a = (int *)malloc(5*sizeof(int));
+	printf("Before Realloc - a : %p\n",a);
 
-  int *b = (int *)malloc(5*sizeof(int));
-  printf("Before Realloc - b : %p\n",b);
+	b = (int *)malloc(5*sizeof(int));
+	printf("Before Realloc - b : %p\n",b);
 
-  a = (int *)realloc(p,10*sizeof(int));
-  printf("After Realloc - a : %p\n",a);
+	//Reallocate DMA & note the address
+	a = (int *)realloc(p,10*sizeof(int));
+	printf("After Realloc - a : %p\n",a);
 
-  //De-Allocate the DMA
-  free(a);
-  free(b);
+	//De-Allocate the DMA
+	free(a);
+	free(b);
 
-  return 0;
+	return 0;
 }
